e8633db1ab59b098b6a0f4b767ada061b8318753
MikhailNahorny <psvm.HelloWorld@gmail.com>
2020-10-07 18:16:15 +0300

    add singleton pattern

---
+++ src/main/java/com/nahorny/pattern/structural/singleton/Singleton1.java
@@ -0,0 +1,21 @@
+/**
+ * не потокобезопасный
+ * ленивый, создает экземпляр по первому запросу
+ */
+
+package com.nahorny.pattern.structural.singleton;
+
+
+public final class Singleton1 {
+    //field to save instance
+    private static Singleton1 instance;
+    //hide constructor
+    private Singleton1() {
+    }
+    //public method return only one instance
+    public static Singleton1 getSingleton() {
+        return instance = (instance == null) ? new Singleton1() : instance;
+    }
+}
+
+
---
+++ src/main/java/com/nahorny/pattern/structural/singleton/Singleton2.java
@@ -0,0 +1,17 @@
+/**
+ * потокобезопасный
+ * нетерпеливый, создает экземпляр сразу
+ */
+package com.nahorny.pattern.structural.singleton;
+
+public final class Singleton2 {
+    //field to save instance
+    private static Singleton2 instance = new Singleton2();
+    //hide constructor
+    private Singleton2() {
+    }
+    //public method return only one instance
+    public static Singleton2 getSingleton() {
+        return instance;
+    }
+}
